9b23692e4461cfffdb2cf04096b4e288a624200a
Roy Derks <roy.derks@ibm.com>
2025-05-16 17:17:30 -0700

    Add agents workshop

---
+++ agents/intro/src/app/actions.ts
@@ -0,0 +1,12 @@
+"use server";
+
+import {
+  mapStoredMessagesToChatMessages,
+  StoredMessage,
+} from "@langchain/core/messages";
+
+export async function message(messages: StoredMessage[]) {
+  const deserialized = mapStoredMessagesToChatMessages(messages);
+
+  return "Please implement the logi";
+}
